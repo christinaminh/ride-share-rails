1d5312db1eae860e75326cf6bd0b8ca1951cafc5
Christina Minh <christina.minh@gmail.com>
2020-11-03 17:00:49 -0800

    unknown

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,28 @@
 class Passenger < ApplicationRecord
+  has_many :trips
+
+  def total_charged
+    if self.trips.empty?
+      total_charged = 0
+    else
+      total_charged = self.trips.sum { |trip| trip.cost }
+    end
+
+    return total_charged
+  end
+
+  #TODO
+  def add_trip(trip)
+    # self.trips.create(trip_hash)?
+    # request_trip in TripsController??
+    self.trips << trip
+  end
+
+  # def request_ride
+  #
+  # end
+  #
+  # def complete_trip
+  #
+  # end
 end
