753233c67c6cf70c73e89efe7d7eafc0cba123b2
Ida <ida.goitom@yahoo.com>
2020-11-03 16:04:48 -0800

    updated index and show methods and views

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,14 +1,22 @@
 class DriversController < ApplicationController
   def index
+    @drivers = Driver.all
   end
 
   def new
+    @driver = Driver.new
   end
 
   def create
   end
 
   def show
+    driver_id = params[:id].to_i
+    @driver = Driver.find_by(id: driver_id)
+    #
+    if @driver.nil?
+      redirect_to drivers_path
+    end
   end
 
   def edit
