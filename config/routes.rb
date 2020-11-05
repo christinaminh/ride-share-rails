5b0dd56a86b0e0a67547406c2117a60dcbcf3db8
Christina Minh <christina.minh@gmail.com>
2020-11-04 16:03:17 -0800

    Added nested route for passenger in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # root to: "homepages#index"
 
   resources :trips
-  resources :passengers
+  resources :passengers do
+    resources :trips, only: [:index, :new]
+  end
   resources :drivers
 
 end
