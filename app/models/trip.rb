7074caffe33834760abd4767eb8b1499983b069e
Christina Minh <christina.minh@gmail.com>
2020-11-05 14:39:12 -0800

    Added validations to Trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,9 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
 
+  validates :driver_id, presence: true
+  validates :passenger_id, presence: true
+  validates :date, presence: true
   validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 6, allow_nil: true}
   validates :cost, numericality: { only_integer: true, greater_than: 0}
 
