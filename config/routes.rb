0c78086204793ed4c4dabe82fa702b51d918850c
Ida <ida.goitom@yahoo.com>
2020-11-05 17:00:36 -0800

    added nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  # root to: "homepages#index"
+  root to: "homepages#index"
 
   resources :trips
   resources :passengers do
     resources :trips, only: [:index, :new]
   end
-  resources :drivers
-
+  resources :drivers do
+    resources :trips, only: [:index, :new]
+  end
+  resources :homepages
   patch '/trips/:id/complete', to: 'trips#complete', as: 'complete_trip'
 end
